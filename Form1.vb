d9e3de88c6146f1f6165c7aca427d4718847580d
wooda <wooda@DADS-DESKTOP>
2025-08-13 13:19:06 -0400

    Updated settings saving on combobox change

--- Form1.vb
+++ Form1.vb
@@ -352,11 +352,9 @@ Public Class Form1
 
     Private Sub cmbBoxGauge1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbBoxGauge1.SelectedIndexChanged
         gaugeOneSensor = cmbBoxGauge1.Text
-        My.Settings.Save()
     End Sub
 
     Private Sub cmbBoxGauge2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbBoxGauge2.SelectedIndexChanged
-        gaugeTwoSensor = cmbBoxGauge2.Text
         My.Settings.Save()
     End Sub
 
